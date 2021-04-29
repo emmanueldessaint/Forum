e428d458bcdb6bd3c2cb7d26947ed63a357b73ea
Emmanuel-Dessaint <73949384+Emmanuel-Dessaint@users.noreply.github.com>
2021-04-29 14:41:53 +0200

    2 bases de données

--- frontend/src/components/Affichagesujets.vue
+++ frontend/src/components/Affichagesujets.vue
@@ -1,9 +1,19 @@
 <template>
     <div class="container">
-        MESSAGE TEST
-        {{id}}
-        {{donnee}}
-       
+        <div class="sujet">
+            <div class="titre">
+            {{donnee[0].titre}}
+            </div>
+            <div class="contenu">
+                <div class="contenu-pseudo">
+                    {{donnee[0].pseudo}}
+                </div>
+                <p class="contenu-message">
+                    {{donnee[0].message}}
+                </p> 
+            </div>
+        </div>
+        
         <div class="sujet" v-for="item in donnee" :key="item.pseudo">
         
         </div>
@@ -50,3 +60,42 @@ export default {
     }
 }
 </script>
+<style>
+.container{
+    margin-top:150px;
+    
+}
+.titre{
+    
+    
+    font-size:2em;
+    margin-right: auto;
+    margin-left: auto;
+
+ 
+}
+.contenu{
+    border:1px solid rgb(182, 182, 182);
+    margin: 10px 50px 10px 50px;
+    min-height:5em;
+    display:flex;
+    flex-direction:row;
+    min-height:4em;
+    background:rgb(233, 233, 233)
+}
+.contenu-pseudo{
+    width:50em;
+    padding-top:8px;
+    font-size:1.3em;
+    border-right: solid 1px rgb(182, 182, 182);
+    background:rgb(233, 233, 233)
+}
+
+.contenu-message{
+    text-align: justify;
+    line-height: 1.5em;
+    padding:8px;
+    /* margin-left:10px; */
+    font-size:1.3em;
+}
+</style>
--- frontend/src/views/Sujet.vue
+++ frontend/src/views/Sujet.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="home">
+        <HeaderPage />
         <Affichagesujets/>
     </div>
 </template>
 <script>
 
+import HeaderPage from '@/components/HeaderPage.vue'
 import Affichagesujets from '@/components/Affichagesujets.vue'
 
 export default {
     name: 'Sujet',
     components : {
+        HeaderPage,
         Affichagesujets,
     }
 }
